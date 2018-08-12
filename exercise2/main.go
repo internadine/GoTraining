7fbe0cefcb53a0f9242f2ed558991bce10d1ec88
Nadine Wischmeier <nadine.wischmeier@outlook.com>
2018-08-12 12:51:54 +0200

    did some exercise

---
+++ exercise2/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var sn int
+	var ln int
+	fmt.Print("Please enter a small number: ")
+	fmt.Scan(&sn)
+	fmt.Print("Please enter a large number: ")
+	fmt.Scan(&ln)
+	fmt.Println(ln % sn)
+
+}
